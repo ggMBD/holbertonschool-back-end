ed944a76c66c662a355b300729be9abe4f681c0d
maouia <maouiabend@gmail.com>
2023-12-18 00:32:15 +0100

    rest api

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+api : first use
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    employee_id = int(sys.argv[1])
+    api_url = "https://jsonplaceholder.typicode.com"
+
+    user_response = requests.get(f"{api_url}/users/{employee_id}")
+    user_data = user_response.json()
+
+    todos_response = requests.get(f"{api_url}/todos?userId={employee_id}")
+    todos_data = todos_response.json()
+
+    employee_name = user_data.get('name')
+    total_tasks = len(todos_data)
+    done_tasks = [task for task in todos_data if task['completed']]
+    total_done_tasks = len(done_tasks)
+
+    print(
+        f"Employee {employee_name} is done with tasks({total_done_tasks}/{total_tasks}):")
+
+    for task in done_tasks:
+        print(f"\t {task['title']}")
